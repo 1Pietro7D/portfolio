808a101633705e082b91caa0037d1794ef7e9132
PietroDanieli <1pie7d@gmail.com>
2023-05-08 06:23:11 +0200

    add some route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Admin\CVController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,19 +23,22 @@ Route::middleware('auth')  //si collega alla cartella middleware
     ->prefix('admin')
     ->group(function () {
         Route::get('/', function () {
-            return view('admin.test');
+            return view('admin.home');});// rotta se utente autenticato
+        Route::prefix('cv')->group(function () {
+            Route::get('/view', [CVController::class, 'view'])->name('cv.view');
+            Route::get('/download', [CVController::class, 'download'])->name('cv.download');
         });
-
-        // Route::get('/restaurants', 'RestaurantController@index')->name('index'); // rotta se utente autenticato
-        // // andiamo a connetterci al controller CRUD associato ai restaurants
-        // Route::resource('restaurants', 'RestaurantController')->parameters(['restaurants'=>'restaurant:slug']);
-        // Route::resource('dishes', 'DishController');
-        // Route::resource('orders', 'OrderController');
+        Route::resource('portfolios', 'PortfolioController')->except('show');
+        Route::resource('sections', 'SectionController')->parameters(['sections'=>'section:slug']);
+        Route::resource('projects', 'ProjectController')->parameters(['projects'=>'project:slug']);
+        Route::resource('skills', 'SkillController')->except(['show','create','edit'])->parameters(['skills'=>'skill:slug']);
+        Route::resource('contacts', 'ContactController')->parameters(['contacts'=>'contact:slug']);
+        Route::resource('icons', 'IconController')->parameters(['icons'=>'icon:slug']);
     });
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('{any?}', function() {  // or any other path returns home.home in view
-    return view("home.home");
- })->where("any", ".*");
+// Route::get('{any?}', function() {  // or any other path returns home.home in view
+//     return view("home.home");
+//  })->where("any", ".*");
 
 
 
