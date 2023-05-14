adbc608e5077b970640e882cd7807de788025f7c
PietroDanieli <1pie7d@gmail.com>
2023-05-15 01:37:08 +0200

    add condition for register

--- resources/views/fragments/header-nav.blade.php
+++ resources/views/fragments/header-nav.blade.php
@@ -1,82 +1,82 @@
-<header>
-    <nav id="nav-header" class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" id="navbarTogglerSupported" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<nav id="nav-header" class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" id="navbarTogglerSupported" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Hidden Side Of Navbar -->
-                <ul class="navbar-nav mr-auto d-block d-md-none nav-hidden">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin/home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.portfolios.index') }}">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.projects.index') }}">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.sections.index') }}">Section</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.skills.index') }}">Skills</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.contacts.index') }}">Contacts</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.icons.index') }}">Icons</a>
-                    </li>
-                </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Hidden Side Of Navbar -->
+            <ul class="navbar-nav mr-auto d-block d-md-none nav-hidden">
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.portfolios.index') }}">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.projects.index') }}">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.sections.index') }}">Section</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.skills.index') }}">Skills</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.contacts.index') }}">Contacts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.icons.index') }}">Icons</a>
+                </li>
+            </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        {{-- @if (Route::has('register'))
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (App\User::count() == 0)
+                        @if (Route::has('register'))
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                        @endif --}}
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                        @endif
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-
-                    @endguest
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">{{-- route('index') --}}
-                            Visita il sito
-                        </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
                     </li>
-                </ul>
-            </div>
+
+                @endguest
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">{{-- route('index') --}}
+                        Visita il sito
+                    </a>
+                </li>
+            </ul>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
